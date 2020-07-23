9ad043715eebc28fc7b6fdb0c9e3fdb0315d3d0c
Shane Runnels <shanerunnels@gmail.com>
2020-07-23 15:47:22 -0500

    fixing bugs

--- Develop/server/routes/api-routes.js
+++ Develop/server/routes/api-routes.js
@@ -2,11 +2,13 @@ const fs = require("fs");
 const tableData = require("../../db/db.json");
 
 module.exports = function(app) {
-    app.get("/api/notes", function(req, res) {
-        // res.send(tableData);
-        fs.readFile("./db/db.json", function(err, data){
-            res.json(data);
-        });
+
+    app.get("/api/notes", function(req, res) {    
+        res.json(tableData);
+    });
+
+    app.get("/api/notes/:id", function(req, res) {
+        res.json(tableData[Number(req.params.id)]);
     });
 
     app.post("/api/notes", function(req, res){
@@ -20,8 +22,19 @@ module.exports = function(app) {
         res.send(tableData);
     });
 
-    // app.delete("/api/notes/:id", function(req, res){
-    //     tableData.length = 0;
-    // });
+    app.delete("/api/notes/:id", function(req, res) {
+
+        let noteId = req.params.id;
+        let newId = 0;
+        tableData = tableData.filter(currentNote => {
+           return currentNote.id != noteId;
+        });
+        for (currentNote of tableData) {
+            currentNote.id = newId.toString();
+            newId++;
+        }
+        fs.writeFileSync("./db/db.json", JSON.stringify(tableData));
+        res.json(tableData);
+    }); 
 }
 
